d7e4b53df757e2ca9b8a6c0998fc52461192bdc7
devarsh kikani <devarshkikani2904@gmail.com>
2023-02-21 18:41:34 +0530

    change bussiness logic of login screen

--- lib/screens/authentication/login/login_screen.dart
+++ lib/screens/authentication/login/login_screen.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:finder/constant/sizedbox.dart';
 import 'package:finder/screens/authentication/login/login_screen_controller.dart';
 import 'package:finder/screens/authentication/registration/registration_screen.dart';
-import 'package:finder/screens/authentication/welcome/welcome_screen.dart';
 import 'package:finder/theme/colors.dart';
 import 'package:finder/theme/text_style.dart';
 import 'package:finder/utils/validator.dart';
@@ -41,118 +40,120 @@ class LoginScreen extends GetView<LoginScreenController> {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                height10,
-                Center(
-                  child: Text(
-                    'Welcome Back!',
-                    style: blackText40.copyWith(
-                      color: primary,
-                      fontFamily: 'source_serif_pro',
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  height10,
+                  Center(
+                    child: Text(
+                      'Welcome Back!',
+                      style: blackText40.copyWith(
+                        color: primary,
+                        fontFamily: 'source_serif_pro',
+                      ),
                     ),
                   ),
-                ),
-                height30,
-                Text(
-                  'Email address',
-                  style: mediumText22.copyWith(color: whiteColor),
-                ),
-                height10,
-                EmailWidget(
-                  hintText: 'Enter your email here',
-                  controller: controller.email,
-                  onChanged: (value) {
-                    if (Validators.validateEmail(
-                                controller.email.text.trim()) ==
-                            null &&
-                        controller.password.text.length >= 6) {
-                      controller.isValid.value = true;
-                    } else {
-                      controller.isValid.value = false;
-                    }
-                  },
-                  textInputAction: TextInputAction.next,
-                ),
-                height20,
-                Text(
-                  'Password',
-                  style: mediumText22.copyWith(color: whiteColor),
-                ),
-                height10,
-                PasswordWidget(
-                  hintText: 'Enter your password here',
-                  passType: 'Password',
-                  showsuffixIcon: true,
-                  controller: controller.password,
-                  onChaged: (value) {
-                    if (Validators.validateEmail(
-                                controller.email.text.trim()) ==
-                            null &&
-                        controller.password.text.length >= 6) {
-                      controller.isValid.value = true;
-                    } else {
-                      controller.isValid.value = false;
-                    }
-                  },
-                ),
-                height15,
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    'Forgot Password?',
-                    style: mediumText14.copyWith(color: whiteColor),
+                  height30,
+                  Text(
+                    'Email address',
+                    style: mediumText22.copyWith(color: whiteColor),
                   ),
-                ),
-                height30,
-                height30,
-                Center(
-                  child: Obx(
-                    () => elevatedButton(
-                      title: 'Sign In',
-                      onTap: controller.isValid.value
-                          ? () {
-                              controller.sendOtp(context);
-                            }
-                          : null,
+                  height10,
+                  EmailWidget(
+                    hintText: 'Enter your email here',
+                    controller: controller.email,
+                    onChanged: (value) {
+                      if (Validators.validateEmail(
+                                  controller.email.text.trim()) ==
+                              null &&
+                          controller.password.text.length >= 6) {
+                        controller.isValid.value = true;
+                      } else {
+                        controller.isValid.value = false;
+                      }
+                    },
+                    textInputAction: TextInputAction.next,
+                  ),
+                  height20,
+                  Text(
+                    'Password',
+                    style: mediumText22.copyWith(color: whiteColor),
+                  ),
+                  height10,
+                  PasswordWidget(
+                    hintText: 'Enter your password here',
+                    passType: 'Password',
+                    showsuffixIcon: true,
+                    controller: controller.password,
+                    onChaged: (value) {
+                      if (Validators.validateEmail(
+                                  controller.email.text.trim()) ==
+                              null &&
+                          controller.password.text.length >= 6) {
+                        controller.isValid.value = true;
+                      } else {
+                        controller.isValid.value = false;
+                      }
+                    },
+                  ),
+                  height15,
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Forgot Password?',
+                      style: mediumText14.copyWith(color: whiteColor),
                     ),
                   ),
-                ),
-                height30,
-                Center(
-                  child: RichText(
-                    textScaleFactor: MediaQuery.of(context).textScaleFactor,
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: '''You don't have an account? ''',
-                          style: regularText16.copyWith(
-                            color: whiteColor,
-                            fontSize: 14,
+                  height30,
+                  height30,
+                  Center(
+                    child: Obx(
+                      () => elevatedButton(
+                        title: 'Send Code',
+                        onTap: controller.isValid.value
+                            ? () {
+                                controller.sendOtp(context);
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                  height30,
+                  Center(
+                    child: RichText(
+                      textScaleFactor: MediaQuery.of(context).textScaleFactor,
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '''You don't have an account? ''',
+                            style: regularText16.copyWith(
+                              color: whiteColor,
+                              fontSize: 14,
+                            ),
                           ),
-                        ),
-                        TextSpan(
-                          text: 'Sign up',
-                          style: regularText16.copyWith(
-                            fontSize: 14,
-                            color: primary,
-                            fontWeight: FontWeight.w500,
-                            decoration: TextDecoration.underline,
+                          TextSpan(
+                            text: 'Sign up',
+                            style: regularText16.copyWith(
+                              fontSize: 14,
+                              color: primary,
+                              fontWeight: FontWeight.w500,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Get.to(
+                                  () => const RegistrationScreen(),
+                                );
+                              },
                           ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Get.to(
-                                () => const RegistrationScreen(),
-                              );
-                            },
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/authentication/login/login_screen_controller.dart
+++ lib/screens/authentication/login/login_screen_controller.dart
@@ -1,16 +1,24 @@
 // ignore_for_file: prefer_foreach
 
+import 'dart:developer';
+
 import 'package:finder/constant/ads_id.dart';
 import 'package:finder/constant/app_endpoints.dart';
 import 'package:finder/constant/show_ads.dart';
+import 'package:finder/constant/storage_key.dart';
+import 'package:finder/models/user_model.dart';
+import 'package:finder/screens/main_home/main_home_screen.dart';
+import 'package:finder/screens/user_info_screen/name_screen.dart';
 import 'package:finder/utils/network_dio.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:finder/screens/authentication/verify_code/verify_code_bindings.dart';
 import 'package:finder/screens/authentication/verify_code/verify_code_screen.dart';
+import 'package:get_storage/get_storage.dart';
 
 class LoginScreenController extends GetxController {
   RxBool isValid = false.obs;
+  GetStorage box = GetStorage();
   final TextEditingController email = TextEditingController();
   final TextEditingController password = TextEditingController();
   final TextEditingController confirmPassword = TextEditingController();
@@ -18,35 +26,52 @@ class LoginScreenController extends GetxController {
   Future<void> sendOtp(BuildContext context) async {
     final ShowAds showAds = ShowAds();
     if (showAds.placements[AdsIds.interstitialVideoAdPlacementId]!.value) {
-      showAds.showAd(
-        AdsIds.interstitialVideoAdPlacementId,
-        () async {
-          await nextFunction(context);
-        },
-      );
+      await nextFunction(context);
     }
   }
 
   Future<void> nextFunction(BuildContext context) async {
     final Map<String, dynamic>? response = await NetworkDio.postDioHttpMethod(
       context: context,
-      url: ApiEndPoints.apiEndPoint + ApiEndPoints.sendOtp,
+      url: ApiEndPoints.apiEndPoint + ApiEndPoints.signIn,
       data: <String, dynamic>{
         'email': email.text.trim(),
         'password': password.text.trim(),
       },
     );
     if (response != null) {
-      NetworkDio.showSuccess(
-        title: 'Success',
-        sucessMessage: 'OTP has been sent successfully to your email',
-      );
-      Get.to(
-        () => const VerifyCodeScreen(),
-        binding: VerifyCodeBinding(
-          phoneNumber: email.text.trim(),
-        ),
-      );
+      if (response['userData']['isEmailVerified'] == true) {
+        log(response['userData'].toString());
+        final UserModel model =
+            UserModel.fromJson(response['userData'] as Map<String, dynamic>);
+        box
+          ..write(StorageKey.apiToken, response['token'])
+          ..write(StorageKey.currentUser, model.toJson())
+          ..write(StorageKey.userId, response['userData']['_id'])
+          ..write(StorageKey.isLogedIn, true);
+        await NetworkDio.setDynamicHeader();
+        if (model.isProfileCompleted) {
+          Get.offAll(
+            () => MainHomeScreen(),
+          );
+        } else {
+          Get.offAll(
+            () => NameScreen(),
+          );
+        }
+      } else {
+        NetworkDio.showSuccess(
+          title: 'Success',
+          sucessMessage: 'OTP has been sent successfully to your email',
+        );
+        Get.to(
+          () => const VerifyCodeScreen(),
+          binding: VerifyCodeBinding(
+            emailAddress: email.text.trim(),
+            isForgot: false,
+          ),
+        );
+      }
     }
   }
 }
