b2fec20eb0e7c9d9545baf7d41040ae66ea95382
devarsh kikani <devarshkikani2904@gmail.com>
2023-02-24 14:11:34 +0530

    redesign chat screen

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -29,127 +29,125 @@ class ChatScreen extends GetView<ChatScreenController> {
                     ),
                   ),
                 ),
-                height20,
+                dividers(10),
+                height10,
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20,
-                    ),
-                    child: Obx(
-                      () => controller.roomsList.isEmpty
-                          ? Center(
-                              child: Text(
-                                'There is no messgae yet.',
-                                style: mediumText18,
-                              ),
-                            )
-                          : ListView.separated(
-                              itemCount: controller.roomsList.length,
-                              physics: const BouncingScrollPhysics(),
-                              shrinkWrap: true,
-                              separatorBuilder:
-                                  (BuildContext context, int index) {
-                                return dividers(10);
-                              },
-                              itemBuilder: (BuildContext context, int index) {
-                                final ChatRoom chatRoomDetails =
-                                    ChatRoom.fromJson(
-                                        controller.roomsList[index]
-                                            as Map<String, dynamic>);
-                                return GestureDetector(
-                                  onTap: () async {
-                                    controller.currentChatRoom =
-                                        chatRoomDetails;
-                                    await controller.getMessages(context);
-                                    await controller.initSocket(controller
-                                        .currentChatRoom.user.id
-                                        .toString());
-                                    Get.to(() => ChatDetaisScreen());
-                                  },
-                                  child: Container(
-                                    margin: const EdgeInsets.symmetric(
-                                      horizontal: 10,
-                                    ),
-                                    color: Colors.transparent,
-                                    child: Row(
-                                      children: <Widget>[
-                                        ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(100),
-                                          child: Image.network(
-                                            chatRoomDetails.user.photos![0]
-                                                .toString(),
-                                            fit: BoxFit.cover,
-                                            height: 40,
-                                            width: 40,
+                  child: Obx(
+                    () => controller.roomsList.isEmpty
+                        ? Center(
+                            child: Text(
+                              'There is no messgae yet.',
+                              style: mediumText18,
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: controller.roomsList.length,
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            padding: EdgeInsets.zero,
+                            itemBuilder: (BuildContext context, int index) {
+                              final ChatRoom chatRoomDetails =
+                                  ChatRoom.fromJson(controller.roomsList[index]
+                                      as Map<String, dynamic>);
+                              return Column(
+                                children: <Widget>[
+                                  GestureDetector(
+                                    onTap: () async {
+                                      controller.currentChatRoom =
+                                          chatRoomDetails;
+                                      await controller.getMessages(context);
+                                      await controller.initSocket(controller
+                                          .currentChatRoom.user.id
+                                          .toString());
+                                      Get.to(() => ChatDetaisScreen());
+                                    },
+                                    child: Container(
+                                      margin: const EdgeInsets.symmetric(
+                                        horizontal: 10,
+                                      ),
+                                      color: Colors.transparent,
+                                      child: Row(
+                                        children: <Widget>[
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(100),
+                                            child: Image.network(
+                                              chatRoomDetails.user.photos![0]
+                                                  .toString(),
+                                              fit: BoxFit.cover,
+                                              height: 40,
+                                              width: 40,
+                                            ),
                                           ),
-                                        ),
-                                        width10,
-                                        Expanded(
-                                          child: Column(
-                                            children: <Widget>[
-                                              Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: <Widget>[
-                                                  Text(
-                                                    '''${chatRoomDetails.user.firstName} ${chatRoomDetails.user.lastName}''',
-                                                    style:
-                                                        mediumText14.copyWith(
-                                                      color: whiteColor,
-                                                      letterSpacing: 0.2,
-                                                    ),
-                                                  ),
-                                                  Container(
-                                                    padding:
-                                                        const EdgeInsets.all(4),
-                                                    decoration:
-                                                        const BoxDecoration(
-                                                      color: primary,
-                                                      shape: BoxShape.circle,
-                                                    ),
-                                                    child: Text(
-                                                      '3',
-                                                      style: regularText12
-                                                          .copyWith(
+                                          width10,
+                                          Expanded(
+                                            child: Column(
+                                              children: <Widget>[
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: <Widget>[
+                                                    Text(
+                                                      '''${chatRoomDetails.user.firstName} ${chatRoomDetails.user.lastName}''',
+                                                      style:
+                                                          mediumText14.copyWith(
                                                         color: whiteColor,
+                                                        letterSpacing: 0.2,
                                                       ),
                                                     ),
-                                                  ),
-                                                ],
-                                              ),
-                                              Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: <Widget>[
-                                                  Text(
-                                                    'plan is going to suffer',
-                                                    style:
-                                                        regularText14.copyWith(
-                                                      color: lightGrey,
+                                                    Container(
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              4),
+                                                      decoration:
+                                                          const BoxDecoration(
+                                                        color: primary,
+                                                        shape: BoxShape.circle,
+                                                      ),
+                                                      child: Text(
+                                                        '3',
+                                                        style: regularText12
+                                                            .copyWith(
+                                                          color: whiteColor,
+                                                        ),
+                                                      ),
                                                     ),
-                                                  ),
-                                                  Text(
-                                                    '5:45 PM',
-                                                    style:
-                                                        regularText12.copyWith(
-                                                      color: lightGrey,
+                                                  ],
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: <Widget>[
+                                                    Text(
+                                                      'plan is going to suffer',
+                                                      style: regularText14
+                                                          .copyWith(
+                                                        color: lightGrey,
+                                                      ),
                                                     ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
+                                                    Text(
+                                                      '5:45 PM',
+                                                      style: regularText12
+                                                          .copyWith(
+                                                        color: lightGrey,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
                                           ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                );
-                              },
-                            ),
-                    ),
+                                  dividers(10)
+                                ],
+                              );
+                            },
+                          ),
                   ),
                 ),
               ],
